a3035a7996d69a1bca00fc7d2fd3771bc858879e
karli <karli@DESKTOP-6839>
2024-04-07 21:09:30 +0200

    Severity Code Description Project File Line Suppression State Error CS0103 The name 'picker' does not exist in the current context Project-K C:\Users\karli\source\repos\Project-K\Project-K\Project-K\Views\UcitelPickerPage.xaml.cs 28 Active
    Severity Code Description Project File Line Suppression State
    Error  'Clicked' is an unexpected token. Expecting white space. Line 49, position 33. Project-K C:\Users\karli\source\repos\Project-K\Project-K\Project-K\Views\UcitelPickerPage.xaml 49

---
+++ Project-K/Project-K/ViewModels/UcitelViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Project_K.ViewModels
+{
+	public class UcitelViewModel : ContentPage
+	{
+		public UcitelViewModel ()
+		{
+			Content = new StackLayout {
+				Children = {
+					new Label { Text = "Welcome to Xamarin.Forms!" }
+				}
+			};
+		}
+	}
+}
---
+++ Project-K/Project-K/Views/UcitelPickerPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static CoreFoundation.DispatchSource;
+using static ZXing.QrCode.Internal.Mode;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms.Shapes;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace Project_K.Views
+{
+    public partial class UcitelPickerPage : ContentPage
+    {
+        public UcitelPickerPage()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var selectedItem = picker.SelectedItem as string;
+            if (selectedItem != null)
+            {
+                // Do something with the selected item
+                DisplayAlert("Selected Item", $"Vybral sis: {selectedItem}", "OK");
+            }
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    
