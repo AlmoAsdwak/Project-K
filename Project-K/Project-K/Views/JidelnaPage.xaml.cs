4616f2033b8f2efa55f398317c135b198b254706
Libor Šenar <liborsenar1@gmail.com>
2024-05-14 18:38:02 +0200

    Added Pages

---
+++ Project-K/Project-K/Views/JidelnaPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Project_K.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class JidelnaPage : ContentPage
+{
+    public JidelnaPage()
+    {
+        InitializeComponent();
+    }
+}
+}
--- Project-K/Project-K/Views/LoginPage.xaml.cs
+++ Project-K/Project-K/Views/LoginPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
@@ -10,11 +11,8 @@ namespace Project_K.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
-        public LoginPage()
-        {
-            InitializeLoginPageAsync();
-        }
-
+        public LoginPage() =>
+            _ = InitializeLoginPageAsync();
         private async Task InitializeLoginPageAsync()
         {
             var Id = await SecureStorage.GetAsync("Id");
@@ -32,50 +30,42 @@ namespace Project_K.Views
             InitializeComponent();
             scannerView.OnScanResult += Result;
         }
-        [Obsolete]
-        void OnPrivacyPolicyButtonClicked(object sender, EventArgs e)
-        {
-            Device.OpenUri(new Uri("http://whoisalmo.cz/soukromi"));
-        }
-        public void Result(ZXing.Result result) =>
+        private async void OnPrivacyPolicyButtonClicked(object sender, EventArgs e) =>
+            await Launcher.OpenAsync(new Uri("http://whoisalmo.cz/soukromi"));
+
+        private void Result(ZXing.Result result) =>
             Device.BeginInvokeOnMainThread(() =>
             {
                 if (result != null && result.BarcodeFormat == ZXing.BarcodeFormat.QR_CODE)
                     Label.Text = result.Text;
             });
 
-        private void HelpButton_Clicked(object sender, EventArgs e) => Label.Text = "Musíš jít do školního informačního systému, do svého profilu, úplně dolů. Kde načteš svůj QR kód.";
+        private void HelpButton_Clicked(object sender, EventArgs e) =>
+            Label.Text = "Musíš jít do školního informačního systému, do svého profilu, úplně dolů. Kde načteš svůj QR kód.";
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            var pattern = "{\"FamilyName\":\"[\\p{L}´ˇ]*\",\"GivenName\":\"[\\p{L}´ˇ]*\",\"Email\":\"\\w+.\\w+@ssakhk.cz\",\"UserId\":\\d+}";
-            var match = Regex.Match(Label.Text, pattern);
-            if (!match.Success)
+            if (!Regex.Match(Label.Text, "{\"FamilyName\":\"[\\p{L}´ˇ]*\",\"GivenName\":\"[\\p{L}´ˇ]*\",\"Email\":\"\\w+.\\w+@ssakhk.cz\",\"UserId\":\\d+}").Success)
             {
                 DisplayAlert("Wrong format", "Youre QR code have bad format", "OK");
                 return;
             }
 
-            var GetFamilyName = Regex.Match(Label.Text, "(?<=\"FamilyName\":\")[\\p{L}´ˇ]*");
-            var GetGivenName = Regex.Match(Label.Text, "(?<=\"GivenName\":\")[\\p{L}´ˇ]*");
-            var GetMail = Regex.Match(Label.Text, "(?<=\\\"Email\\\":\")\\w+.\\w+@ssakhk.cz");
-            var GetId = Regex.Match(Label.Text, "(?<=\"UserId\":)\\d+");
+            var Informations = JsonSerializer.Deserialize<LoginInformation>(Label.Text);
 
-            SecureStorage.SetAsync("FamilyName", GetFamilyName.ToString()).Wait();
-            SecureStorage.SetAsync("GivenName", GetGivenName.ToString()).Wait();
-            SecureStorage.SetAsync("Mail", GetMail.ToString()).Wait();
-            SecureStorage.SetAsync("Id", GetId.ToString()).Wait();
-
-            var userData = SecureStorage.GetAsync("Id").Result;
-            if (userData == null)
-            {
-                DisplayAlert("Wrong format", "Youre QR code have bad format", "OK");
-                SecureStorage.RemoveAll();
-                return;
-            }
+            SecureStorage.SetAsync("FamilyName", Informations.FamilyName).Wait();
+            SecureStorage.SetAsync("GivenName", Informations.GivenName).Wait();
+            SecureStorage.SetAsync("Mail", Informations.Email).Wait();
+            SecureStorage.SetAsync("Id", Informations.UserId.ToString()).Wait();
 
             Application.Current.MainPage = new AppShell();
         }
+        class LoginInformation
+        {
+            public string FamilyName { get; set; }
+            public string GivenName { get; set; }
+            public string Email { get; set; }
+            public int UserId { get; set; }
+        }
     }
-
 }
