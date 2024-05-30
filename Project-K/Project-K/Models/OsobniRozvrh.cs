2ba633f77dcbe53e5d4bdb16731287e15c37c455
senar.libor@ssakhk.cz <senar.libor@ssakhk.cz>
2024-05-30 09:43:28 +0200

    Added Volno moment

--- Project-K/Project-K/Models/OsobniRozvrh.cs
+++ Project-K/Project-K/Models/OsobniRozvrh.cs
@@ -6,24 +6,36 @@ namespace Project_K.Models
     {
         public Cell[] Cells { get; set; }
     }
+
     public class Cell
     {
-        private DateTime _startTime;
+        public DateTime UnformattedStartTime;
         public string GroupName { get; set; }
         public string Subject { get; set; }
         public string Teacher { get; set; }
         public string ClassRoom { get; set; }
         public string MeetUrl { get; set; }
+        public string TimeTo { get; set; }
+
         public string StartTime
         {
-            get { return _startTime.ToString("yyyy-MM-ddTHH:mm:ss"); }
+            get => UnformattedStartTime.ToString("yyyy-MM-ddTHH:mm:ss");
             set
             {
                 if (DateTime.TryParse(value, out DateTime startTime))
-                    _startTime = startTime;
+                    UnformattedStartTime = startTime;
             }
         }
-        public string FormattedStartTime => _startTime.ToString("HH:mm");
-        public string TimeTo => _startTime.AddMinutes(45).ToString("HH:mm");
+
+        public string FormattedStartTime
+        {
+            get => UnformattedStartTime.ToString("HH:mm");
+            set
+            {
+                if (DateTime.TryParse(value, out DateTime formattedStartTime))
+                    UnformattedStartTime = formattedStartTime;
+
+            }
+        }
     }
 }
