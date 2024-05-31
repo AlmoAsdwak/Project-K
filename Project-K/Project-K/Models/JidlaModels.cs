6e9f247983ac2870d8c92843b18b4844d2f31107
senar.libor@ssakhk.cz <senar.libor@ssakhk.cz>
2024-05-31 09:45:56 +0200

    Jidelna login done

---
+++ Project-K/Project-K/Models/JidlaModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_K.Models
+{
+    internal class JidlaModels
+    {
+        public class Rootobject
+        {
+            public Day[] days { get; set; }
+        }
+
+        public class Day
+        {
+            public string date { get; set; }
+            public Food[] foods { get; set; }
+        }
+
+        public class Food
+        {
+            public int type { get; set; }
+            public string name { get; set; }
+            public bool ordered { get; set; }
+        }
+    }
+}
--- Project-K/Project-K/Views/JidelnaPage.xaml.cs
+++ Project-K/Project-K/Views/JidelnaPage.xaml.cs
@@ -1,14 +1,77 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using System.Net.Http;
+using System.Text.Json;
+using System.Net;
+using Project_K.Models;
 
 namespace Project_K.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class JidelnaPage : ContentPage
-{
-    public JidelnaPage()
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class JidelnaPage : ContentPage
     {
-        InitializeComponent();
+        public JidelnaPage()
+        {
+            InitializeComponent();
+            var JidelnaSid = SecureStorage.GetAsync("JidelnaSid").Result;
+            if (JidelnaSid == null)
+            {
+                LoginText.IsVisible = true;
+                loginForm.IsVisible = true;
+                submitForm.IsVisible = true;
+                return;
+            }
+        }
+
+        private void loginButton_Clicked(object sender, System.EventArgs e)
+        {
+            if (usernameEntry.Text == null || passwordEntry.Text == null)
+            {
+                DisplayAlert("Error", "You must fill all fields", "OK");
+                return;
+            }
+            HttpClient client = new HttpClient();
+            var data = new { userId = usernameEntry.Text, password = passwordEntry.Text };
+            var dataJson = JsonSerializer.Serialize(data);
+            var response = client.PostAsync("https://jidelna.farhive.cz/api/v1/Login", new StringContent(dataJson, System.Text.Encoding.UTF8, "application/json")).Result;
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                DisplayAlert("Error", "Wrong username or password", "OK");
+                return;
+            }
+            var responseString = response.Content.ReadAsStringAsync().Result;
+            var dataJson2 = JsonSerializer.Deserialize<DataJson>(responseString);
+            SecureStorage.SetAsync("JidelnaSid", dataJson2.sid).Wait();
+            if (rememberMe.IsChecked)
+            {
+                SecureStorage.SetAsync("JidelnaUsername", usernameEntry.Text).Wait();
+                SecureStorage.SetAsync("JidelnaPassword", passwordEntry.Text).Wait();
+            }
+            Shell.Current.GoToAsync("//JidelnaPage").Wait();
+        }
+        class DataJson
+        {
+            public string sid { get; set; }
+        }
+
+        public class Rootobject
+        {
+            public Day[] days { get; set; }
+        }
+
+        public class Day
+        {
+            public string date { get; set; }
+            public Food[] foods { get; set; }
+        }
+
+        public class Food
+        {
+            public int type { get; set; }
+            public string name { get; set; }
+            public bool ordered { get; set; }
+        }
+
     }
-}
 }
